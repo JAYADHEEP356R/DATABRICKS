1083de05c4ea7d7cfd557a08edb6a0b3f6d6bcbb
JAYADHEEP356R <jayadheep.r@diggibyte.com>
2026-03-22 11:50:28 +0000

    date_functions

--- src/Dlt_Demo/transformations/dlt_sourceCode.py
+++ src/Dlt_Demo/transformations/dlt_sourceCode.py
@@ -11,14 +11,12 @@ from pyspark.sql.types import *
     comment="Raw orders data from CSV"
 )
 def bronze_orders():
-
     return (
         spark.read
         .option("header", True)
         .option("inferSchema", True)
         .csv("/Volumes/workspace/default/autoloads/raw_data/orders_batch1.csv")
     )
-
 
 # ---------------------------
 # SILVER TABLE (CLEANING)
@@ -28,19 +26,21 @@ def bronze_orders():
     name="silver_orders",
     comment="Cleaned orders data"
 )
-
 @dlt.expect("valid_amount", "order_amount > 0")
-
 def silver_orders():
-
     df = dlt.read("bronze_orders")
-
     return (
         df.withColumn("order_date", to_date(col("order_date"), "yyyy-MM-dd"))
         .withColumn("order_amount", col("order_amount").cast("double"))
         .filter(col("order_status").isNotNull())
     )
 
+@dlt.view(name="silver_orderid_view")
+def silver_orderid_view():
+    df = dlt.read("silver_orders")
+    return (
+        df.select("order_id")
+    )
 
 # ---------------------------
 # GOLD TABLE (BUSINESS LOGIC)
@@ -50,11 +50,8 @@ def silver_orders():
     name="gold_orders_summary",
     comment="Aggregated order metrics"
 )
-
 def gold_orders_summary():
-
     df = dlt.read("silver_orders")
-
     return (
         df.groupBy("order_status")
         .agg(
